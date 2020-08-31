b7ca4e30d33694ed8ff4cfc8648100041491870f
Sandy_undefined <08sandysk@gmail.com>
2020-08-31 05:42:00 +0530

    Yahoo bug!

--- SearchEngine/SearchEngine/search.py
+++ SearchEngine/SearchEngine/search.py
@@ -4,51 +4,61 @@ from bs4 import BeautifulSoup
 
 #done
 def google(s):
-    results = []
+    links = []
+    text = []
     USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
     headers = {"user-agent": USER_AGENT}
     r = requests.get("https://www.google.com/search?q=" +s, headers=headers)
     soup = BeautifulSoup(r.content, "html.parser")
     for g in soup.find_all('div', class_='r'):
         a = g.find('a')
-        results.append(a.get('href'))
-    return results
+        t = g.find('h3')
+        links.append(a.get('href'))
+        text.append(t.text)
+    return links,text
 
 
 #done
 def yahoo(s):
-    result = []
+    links = []
+    text = []
     url = "https://search.yahoo.com/search?q=" +s+ "&n=" + str(10)
     raw_page = requests.get(url)
+    print(raw_page)
     soup = BeautifulSoup(raw_page.text, "html.parser")
     for link in soup.find_all(attrs={"class": "ac-algo fz-l ac-21th lh-24"}):
-        result.append(link.get('href'))
-    return result
+        links.append(link.get('href'))
+        text.append(link.text)
+    return links,text
 
 
 #done
 def duck(s):
-    results = []
+    links = []
+    text = []
     userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
     headers = {'user-agent': userAgent}
     r = requests.get('https://duckduckgo.com/html/?q=' + s, headers=headers)
     s = BeautifulSoup(r.content, "html.parser")
     for i in s.find_all('div', attrs={'class': 'results_links_deep'}):
         a = i.find('a',attrs={'class': 'result__a'})
-        results.append(a.get('href'))
-    results.pop(0)
-    return results
+        links.append(a.get('href'))
+        text.append(a.text)
+    links.pop(0)
+    text.pop(0)
+    return links,text
 
 
 #done
 def ecosia(s):
-    results = []
+    links = []
+    text = []
     userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
     headers = {'user-agent': userAgent}
     r = requests.get('https://www.ecosia.org/search?q=' + s, headers=headers)
     soup = BeautifulSoup(r.text, "html.parser")
     for i in soup.find_all("h2", attrs={'class': 'result-firstline-title'}):
         a = i.find("a", attrs={'class': 'js-result-title'})
-        #print(a.text)
-        results.append(a.get('href'))
-    return results
+        text.append(a.text)
+        links.append(a.get('href'))
+    return links,text
--- SearchEngine/SearchEngine/views.py
+++ SearchEngine/SearchEngine/views.py
@@ -9,10 +9,14 @@ def homepage(request):
 def results(request):
     if request.method == "POST":
         result = request.POST.get('search')
-        google_data = google(result)
-        yahoo_data = yahoo(result)
-        duck_data = duck(result)
-        ecosia_data = ecosia(result)
+        google_link,google_text = google(result)
+        google_data = zip(google_link,google_text)
+        yahoo_link,yahoo_text = yahoo(result)
+        yahoo_data = zip(yahoo_link,yahoo_text)
+        duck_link,duck_text = duck(result)
+        duck_data = zip(duck_link,duck_text)
+        ecosia_link,ecosia_text = ecosia(result)
+        ecosia_data = zip(ecosia_link,ecosia_text)
         if result == '':
             return redirect('Home')
         else:
