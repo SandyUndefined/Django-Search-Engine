fae3f2b3122b9e0bb6b44df0e04b1a6ab70a517e
Sandy_undefined <08sandysk@gmail.com>
2021-06-10 10:05:01 +0530

    deploy

--- SearchEngine/settings.py
+++ SearchEngine/settings.py
@@ -122,4 +122,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [STATIC_DIR]
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
